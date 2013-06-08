c29fb58f5efe59dc973bb25dd522fdcc5adae51e
Łukasz Niemier <lukasz@niemier.pl>
2013-06-08 14:53:35 +0200

    Restyle models and add numericality validation to `amount`

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,15 +1,20 @@
 class Transaction
   include Mongoid::Document
   include Mongoid::Timestamps
+
   field :user_id, type: Integer
   field :name, type: String
   field :amount, type: BigDecimal
-  field :kind, type: String
+  field :kind, type: Symbol
+
+  validates_numericality_of :amount
 
   belongs_to :user
 
   def kind_class
-  	return "success" if kind == "income"
-  	return "error" if kind == "outcome"
+    case kind
+    when :income then "success"
+    when :outcome then "error"
+    end
   end
 end
