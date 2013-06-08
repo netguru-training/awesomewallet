3d500985a5df3a5b6de9cddb33b389d0beb99ce9
Łukasz Niemier <lukasz@niemier.pl>
2013-06-08 14:37:56 +0200

    Authenticate user before showing his transactions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,24 +5,27 @@ class ApplicationController < ActionController::Base
   helper_method :correct_user?
 
   private
-    def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id].present?
-    end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id].present?
+  end
 
-    def signed_in?
-      !!current_user
-    end
-    helper_method :current_user, :signed_in?
+  def signed_in?
+    !!current_user
+  end
+  helper_method :current_user, :signed_in?
 
-    def current_user=(user)
-      @current_user = user
-      session[:user_id] = user.nil? ? user : user.id
-    end
+  def current_user=(user)
+    @current_user = user
+    session[:user_id] = user.nil? ? user : user.id
+  end
 
-    def authenticate_user!
-      if !current_user
-        redirect_to root_url, :alert => 'You need to sign in for access to this page.'
-      end
+  def authenticate_user!
+    if !current_user
+      redirect_to root_url, alert: 'You need to sign in for access to this page.'
     end
+  end
 
+  def self.authenticate_user!
+    before_filter :authenticate_user!
+  end
 end
