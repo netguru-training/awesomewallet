b0e69c7e7aa3e08e611b646935faa3ecb94e8ac3
Igor Dominiak <dominiak.igor@gmail.com>
2013-06-08 11:56:08 +0200

    Add identity model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Awesomewallet::Application.routes.draw do
-	root to: 'dashboard#index'
-
-	resources :transactions
+  root to: 'dashboard#index'
+  resources :transactions
+  match '/auth/:provider/callback', to: 'sessions#create'
+  match '/logout', to: 'sessions#destroy'
 end
