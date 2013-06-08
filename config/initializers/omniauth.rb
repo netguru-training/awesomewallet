1052f4fa3ddfefb70f9f95b72bf1a3b3d43e6847
Igor Dominiak <dominiak.igor@gmail.com>
2013-06-08 11:13:52 +0200

    add google keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, '73a3698de16e3be8c6d0', '2a03ca8180e5655d48395304b15f8abe34b84d67'
-  provider :google_oauth2, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  provider :google_oauth2, "1001644837540.apps.googleusercontent.com","Vhfdr24B3SJFFgKbt-3lmZCk"
 end
