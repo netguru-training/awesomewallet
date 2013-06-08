26abe2f17bc01486c66cc63976fe9c88fbd3933e
Igor Dominiak <dominiak.igor@gmail.com>
2013-06-08 13:46:44 +0200

    update transaction controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,3 +1,18 @@
 class TransactionsController < ApplicationController
-	expose(:transactions)
+	expose(:transactions) { current_user.transactions }
+
+	def create
+		render :edit return unless transaction.save
+			redirect_to transactions_path, notice: "Transaction created!"
+	end
+
+	def update
+		render :edit return unless transaction.save
+			redirect_to transactions_path, notice: "Transaction updated!"
+	end
+
+	def destroy
+		transaction.destroy
+		redirect_to transactions_path, notice: "Transaction destroyed!"
+	end
 end
