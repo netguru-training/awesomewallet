d8eba53197bd10e8fd8ba797a44995b0ba407f6b
gosiakowata <janinska@me.com>
2013-06-08 13:43:27 +0200

    Change amout type in transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,6 +3,6 @@ class Transaction
   include Mongoid::Timestamps
   field :user_id, type: Integer
   field :name, type: String
-  field :amount, type: Integer
+  field :amount, type: BigDecimal
   field :kind, type: String
 end
