fd409e7944cfa4e5418418b3f7408babbe6187a9
busiq <mbusiq@gmail.com>
2013-06-08 10:41:11 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awesomewallet::Application.config.secret_token = 'f3bf4fe779e042e93efc89267ccfda5a6633468d9e4092497dc5c1ef16e86a159e550ad4e61d462941fd7cea0ce42b3359959ddc9bd703c83ffbdd946345b377'
