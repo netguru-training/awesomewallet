562bddf0fb9904460eb8cdf79fdafe95cf62cabd
gosiakowata <janinska@me.com>
2013-06-08 14:55:14 +0200

    Add validations to transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,6 +5,10 @@ class Transaction
   field :name, type: String
   field :amount, type: BigDecimal
   field :kind, type: String
+  field :current_balance, type: BigDecimal
+
+  validates :amount, numericality: true
+  validates :name, :kind, :amount, :user_id, :current_balance, presence: true
 
   belongs_to :user
 end
