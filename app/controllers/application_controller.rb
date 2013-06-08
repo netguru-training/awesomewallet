b8f778889ca5005f424fb89c6f7cac859f2cd588
busiq <mbusiq@gmail.com>
2013-06-08 13:14:24 +0200

    fix fix fix :arrhhh:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,14 +10,13 @@ class ApplicationController < ActionController::Base
     end
 
     def signed_in?
-      
       !!current_user
     end
     helper_method :current_user, :signed_in?
 
     def current_user=(user)
       @current_user = user
-      session[:user_id] = user.id
+      session[:user_id] = user.nil? ? user : user.id
     end
 
     def authenticate_user!
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, '73a3698de16e3be8c6d0', '2a03ca8180e5655d48395304b15f8abe34b84d67'
+  provider :github, '73a3698de16e3be8c6d0', '2a03ca8180e5655d48395304b15f8abe34b84d67', scope: 'user'
   provider :google_oauth2, "1001644837540.apps.googleusercontent.com","Vhfdr24B3SJFFgKbt-3lmZCk"
   provider :facebook, "587450764608725", "210bf438ba7749541f21401cbc70ab52"
 end
