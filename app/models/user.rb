45240e140c119fae3f9413a0cec1c3613cdcc14f
Igor Dominiak <dominiak.igor@gmail.com>
2013-06-08 13:10:11 +0200

    fix user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User
   include Mongoid::Document
   field :name, type: String
   field :email, type: String
+  field :balance, type: Integer
   attr_accessible :name, :email
   has_many :identities
   validates_presence_of :name
@@ -9,6 +10,8 @@ class User
   index({ email: 1 }, { unique: true, background: true })
 
   def self.create_with_omniauth(auth)
+    user = User.where(email: auth['info']['email']).first
+    return user if user.present?
     create! do |user|
       if auth['info']
          user.name = auth['info']['name'] || ""
