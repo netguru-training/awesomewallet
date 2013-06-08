22910d12e844c68d50fbc42154485d6959e536a1
busiq <mbusiq@gmail.com>
2013-06-08 13:00:23 +0200

    sessions fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,13 +10,14 @@ class ApplicationController < ActionController::Base
     end
 
     def signed_in?
+      
       !!current_user
     end
     helper_method :current_user, :signed_in?
 
     def current_user=(user)
       @current_user = user
-      session[:user_id] = user.nil? ? user : user.id
+      session[:user_id] = user.id
     end
 
     def authenticate_user!
