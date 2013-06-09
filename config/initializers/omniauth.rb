fa8c63512ddf02a012a507f63cc5bdc146b7f094
busiq <mbusiq@gmail.com>
2013-06-09 11:57:41 +0200

    add config files

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, '73a3698de16e3be8c6d0', '2a03ca8180e5655d48395304b15f8abe34b84d67', scope: 'user'
-  provider :google_oauth2, "1001644837540.apps.googleusercontent.com","Vhfdr24B3SJFFgKbt-3lmZCk"
-  provider :facebook, "587450764608725", "210bf438ba7749541f21401cbc70ab52"
+  provider :github, AppConfig.auth_keys.github_id, AppConfig.auth_keys.github_secret, scope: 'user'
+  provider :google_oauth2, AppConfig.auth_keys.google_id,AppConfig.auth_keys.google_secret
+  provider :facebook, AppConfig.auth_keys.facebook_id, AppConfig.auth_keys.facebook_secret
 end
